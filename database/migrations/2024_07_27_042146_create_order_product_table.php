fc2bc98d0dfa33a4e9ca9a7ed33eaf27a00f88c9
Yoan Estrada Blanco <yoanestrada0102@gmail.com>
2024-08-01 19:03:03 -0400

    fix: fixed bug namening convencional migrations

--- database/migrations/2024_07_27_042146_create_order_product_table.php
+++ database/migrations/2024_07_27_042146_create_order_product_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('order_products', function (Blueprint $table) {
+        Schema::create('order_product', function (Blueprint $table) {
             $table->id();
             $table->foreignId('order_id')->constrained()->cascadeOnDelete();
             $table->foreignId('product_id')->constrained()->cascadeOnDelete();
