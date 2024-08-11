a5b022da24d772041c652904e526b05d6ab51612
Yoan Estrada Blanco <yoanestrada0102@gmail.com>
2024-08-10 22:33:42 -0400

    feat: add lógic frontend summary

--- resources/views/livewire/layout/summary.blade.php
+++ resources/views/livewire/layout/summary.blade.php
@@ -6,11 +6,14 @@ use Gloudemans\Shoppingcart\Facades\Cart;
 on(['addCart']);
 on(['destroy_cart']);
 
+$addQty = fn($rowId, $quantity) => Cart::update($rowId, $quantity + 1);
+$subtractQty = fn($rowId, $quantity) => Cart::update($rowId, $quantity - 1);
+$removeProduct = fn($rowId) => Cart::remove($rowId);
 ?>
 
 <div class="bg-rose-100 p-4 md:w-72">
     <div class="min-h-72 rounded-md bg-white p-2">
-        <h2 class="mb-2 text-3xl font-bold text-red-base">Tu Carrito ({{ Cart::count() }})</h2>
+        <h2 class="mb-2 text-3xl font-black text-red-base">Tu Carrito ({{ Cart::count() }})</h2>
         @if (Cart::count() == 0)
             <div class="mt-2 flex flex-col items-center justify-center">
                 <img class="mb-3" src="{{ asset('storage/icons/illustration-empty-cart.svg') }}" alt="carrito vácio">
@@ -30,27 +33,35 @@ on(['destroy_cart']);
                         </div>
                         <div class="flex items-center justify-between px-4 py-2">
                             <div class="flex h-7 w-6/12 items-center justify-between rounded-3xl bg-red-base p-2">
-                                <button class="h-4 w-4 rounded-full border border-slate-100 p-0.5">
+                                <button class="h-4 w-4 rounded-full border border-slate-100 p-0.5"
+                                    wire:click="subtractQty('{{ $product->rowId }}', {{ $product->qty }})">
                                     <img src="{{ asset('storage/icons/icon-decrement-quantity.svg') }}"
                                         alt="eliminar producto">
                                 </button>
-                                <button class="h-4 w-4 rounded-full border border-slate-100 p-0.5">
+                                <button class="h-4 w-4 rounded-full border border-slate-100 p-0.5"
+                                    wire:click="addQty('{{ $product->rowId }}', {{ $product->qty }})">
                                     <img src="{{ asset('storage/icons/icon-increment-quantity.svg') }}"
-                                        alt="eliminar producto">
+                                        alt="incrementar producto">
                                 </button>
                             </div>
-                            <button class="h-4 w-4 rounded-full border border-slate-500 p-0.5">
+                            <button class="h-4 w-4 rounded-full border border-slate-500 p-0.5"
+                                wire:click="removeProduct('{{ $product->rowId }}')">
+
                                 <img src="{{ asset('storage/icons/icon-remove-item.svg') }}" alt="eliminar producto">
                             </button>
                         </div>
                     </article>
-                    {{-- <livewire:components.product-cart :$product :key="$product->rowId"> --}}
                 @endforeach
+                <hr class="mt-4 shadow" />
+                <div class="flex items-center justify-between px-2 py-4">
+                    <p>Total a Pagar: </p>
+                    <p class="mt-2 text-xl font-black">${{ Cart::subtotal() }}</p>
+                </div>
+                <button
+                    class="w-full rounded-3xl bg-red-base px-2 py-3 text-white transition-colors hover:bg-red-700">Confirmar
+                    la
+                    orden</button>
 
-                {{-- <p>SubTotal: ${{ Cart::subtotal() }}</p>
-                <p>Domicilio: ${{ Cart::tax() }}</p>
-                <p>Total ${{ Cart::total() }}</p>
-                {{ Cart::priceTotal() }} --}}
             </div>
         @endif
 
