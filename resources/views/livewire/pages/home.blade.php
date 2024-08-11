47063672aa46c8bbdf616f49bc69df8c17ae3236
Yoan Estrada Blanco <yoanestrada0102@gmail.com>
2024-08-10 23:19:21 -0400

    feat: add Heading indicator category dinamic

--- resources/views/livewire/pages/home.blade.php
+++ resources/views/livewire/pages/home.blade.php
@@ -3,9 +3,11 @@
 use function Livewire\Volt\{state, mount, layout, on};
 
 use App\Business\IProductRepository;
+use App\Business\ICategoryRepository;
 
 state([
     'products' => [],
+    'categoryName' => 'Todos los productos',
 ]);
 
 mount(function (IProductRepository $product) {
@@ -13,7 +15,8 @@ mount(function (IProductRepository $product) {
 });
 
 on([
-    'filter-products' => function ($categoryId, IProductRepository $product) {
+    'filter-products' => function ($categoryId, IProductRepository $product, ICategoryRepository $categoryS) {
+        $this->categoryName = $categoryS->getById($categoryId)->name;
         $this->products = $product->getByCategory($categoryId);
     },
 ]);
@@ -23,7 +26,7 @@ layout('layouts.default');
 ?>
 
 <div>
-    <h1 class="text-4xl font-black">Inicio</h1>
+    <h1 class="text-4xl font-black">{{ $categoryName }}</h1>
     <p class="my-4 text-2xl">Elige y perzonaliza tu pedido a continuación</p>
     <div class="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
         @forelse ($products as $product)
