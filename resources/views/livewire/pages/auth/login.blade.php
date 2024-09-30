95ec8d30bfee54a44c9423fc311db8a1e0efc87a
Yoan Estrada Blanco <yoanestrada0102@gmail.com>
2024-09-30 10:26:06 -0400

    feat: add link to register

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -62,8 +62,8 @@ $login = function () {
         <div class="mt-4 flex items-center justify-end">
             @if (Route::has('password.request'))
                 <a class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800"
-                    href="{{ route('password.request') }}" wire:navigate>
-                    {{ __('¿Olvidaste tu contraseña?') }}
+                    href="{{ route('register') }}" wire:navigate>
+                    {{ __('¿Todavía no tienes cuenta?') }}
                 </a>
             @endif
 
